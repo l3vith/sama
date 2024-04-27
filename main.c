04c3b1ad0af28206297d5e860b160a220513dfc1
l3vith <smitbarve666@gmail.com>
2024-04-27 21:32:18 +0530

    giving up for today

--- main.c
+++ main.c
@@ -40,24 +40,45 @@ struct heapchunk_t {
   uint32_t size;
   bool inuse;
   struct heapchunk_t *next;
+};
+
+int init_heap(struct heapinfo_t *heap) {
+  // Allocating Page Size Memory (4096 ????)
+  void *start = mmap(NULL, getpagesize(), PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  if (start == (void *)-1) {
+    perror("Memory Allocation Failed");
+    return (errno);
+  }
+
+  // Subtracting Struct Size from Page Size
+  struct heapchunk_t *first = (struct heapchunk_t *)start;
+  first->size = getpagesize() - sizeof(struct heapchunk_t);
+  first->inuse = false;
+
+  heap->start = first;
+  // heap->start->next =
+  //(struct heapchunk_t *)(start + sizeof(struct heapchunk_t) + first->size);
+  heap->avail = first->size;
+  return 0;
 }
 
+struct heapinfo_t heap = {0};
 
-
-
-
-
+int *heap_alloc(size_t size) {
+  if (size > heap.avail) {
+    printf("Insufficient Size\n");
+    return (void *)-1;
+  }
+  return 0;
+}
 
 int main() {
-  Block mem = allocate(getpagesize(), PROT_READ | PROT_WRITE,
-                       MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  printf("Memory add: %p", mem.address);
+  if (init_heap(&heap) != errno) {
+    printf("Failed to initialized heap\n");
+    return -1;
+  }
 
-  int *alpha = (int *)mem.address;
-  alpha[0] = 100;
-  printf("\nValue is %d", *alpha++);
-
-  printf("\nSize of allocated: %lu", mem.size);
-  printf("PID: %d\n", getpid());
+  printf("Allocation: %d", *heap_alloc(10));
   return 0;
 }
